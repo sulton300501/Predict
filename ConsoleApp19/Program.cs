65f5aa1d2f7d44d02921abf3e28c434bd6f3cbd0
sulton <global-sultonmalik326@gmail.com>
2023-12-26 19:29:05 +0500

    Add project files.

---
+++ ConsoleApp19/Program.cs
@@ -0,0 +1,92 @@
+﻿namespace ConsoleApp19
+{
+    internal class Program
+    {
+
+
+
+        public delegate TResult Func<in item1, in item2, out TResult>(item1 arg, item2 arg2);
+
+        static void Main(string[] args)
+        {
+
+
+
+
+
+            Action myDekegate = () =>
+            {
+                int sum2 = 0;
+                for (int i = 0; i < 10; i++)
+                {
+                    sum2 += i;
+                }
+
+                Console.WriteLine(sum2);
+
+
+
+            };
+
+
+            Action<int> myDeke = delegate (int x)
+            {
+
+                int sum = 0;
+                for (int i = 0; i < x; i++)
+                {
+                    sum += i;
+                }
+
+                Console.WriteLine(sum);
+
+
+            };
+
+
+
+            Predicate<Tuple<int, int, int>> isUchburchak = isTriangle;
+
+            Tuple<int,int,int> x = new Tuple<int,int,int>(3,4,5);
+
+            Console.WriteLine(isUchburchak(x));
+        }
+
+        static bool isTriangle(Tuple<int, int, int> myTuple)
+        {
+            if (Math.Pow(myTuple.Item1, 2) + Math.Pow(myTuple.Item2, 2) == Math.Pow(myTuple.Item3, 2))
+            {
+                return true;
+            }
+            return false;
+
+        }
+
+
+
+
+
+
+        static void Add()
+        {
+            Console.WriteLine("Mening methodim ishlayapti...");
+        }
+
+        static int Add2()
+        {
+            int sum = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                sum += i;
+            }
+            return sum;
+        }
+
+
+
+
+
+    }
+}
+
+
---
+++ ConsoleApp19/ProjectName.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp19
+{
+    public class ProjectName
+    {
+        public double Add(int a , int b)
+        {
+            return a + b;
+        }
+
+
+    }
+}
